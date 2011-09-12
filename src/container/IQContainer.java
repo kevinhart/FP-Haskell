f5ef98ffffe52b73c7448bf0eb2e03a21410d8bd
Matt MacEwan(GLaDOS) <mrm4677@rit.edu>
2011-09-12 14:27:45 -0400

    Interface for basic container
    IQContainer specifies a container which supports where and select.

---
+++ src/container/IQContainer.java
@@ -0,0 +1,29 @@
+package container;
+
+import delegate.*;
+
+/**
+ * Basic queryable container interface.  Supports where and select.
+ * @author mrm4677
+ *
+ * @param <T> Type of contained objects.
+ */
+public interface IQContainer<T>
+{
+	/**
+	 * Filters this container by the given predicate.
+	 * @param pred
+	 * @return a new container containing only the objects for which the
+	 * predicate returns <code>true</code>
+	 */
+	IQContainer<T> Where(IPredicate<T> pred);
+	
+	/**
+	 * Produces a result container by applying the delegate to all elements.
+	 * @param <U> Type of the result of the selector
+	 * @param selector a delegate which transforms the contents of
+	 * this container
+	 * @return a new container
+	 */
+	<U> IQContainer<U> Select(IDelegate<U, T> selector);
+}
