d4b764ab0675906eb4109a97352f2893e8903f4a
Kevin Hart <kevin@k-hart.com>
2011-09-14 00:43:31 -0400

    Added simplified "cs people" container and implemented sample query.

--- src/main/PartA.java
+++ src/main/PartA.java
@@ -7,13 +7,15 @@ import delegate.*;
 public class PartA 
 {
 	public static QueryableArrayList<Person> people;
-
+	public static QueryableArrayList<Person> cs_people;
+	
 	/**
 	 * Delegate test
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
+		// some people
 		people = new QueryableArrayList<Person>();
 		people.add(new Person("Billy", "Mays", 5574824, true));
 		people.add(new Person("Vince", "Offer", 7426969, true));
@@ -21,6 +23,13 @@ public class PartA
 		people.add(new Person("Oprah", "Winfrey", 2222222, false));
 		people.add(new Person("April", "Showers", 5938521, false));
 
+		// some cs people
+		cs_people = new QueryableArrayList<Person>();
+		cs_people.add(new Person("Sandy", "Ferrara", 55178, false));
+		cs_people.add(new Person("Tina", "Sturgis", 57905, false));
+		cs_people.add(new Person("Jason", "Harrison", 52529, false));
+		cs_people.add(new Person("Sam", "Waters", 54934, false));
+		
 		// print people with even phone numbers
 		printList(people.Where(new IPredicate<Person>(){
 			public Boolean execute(Person p) { return p.phone % 2 == 0; }
@@ -53,6 +62,17 @@ public class PartA
 			public Object execute(Person p) { return p.first; }
 		}).Sort( new ISortDelegate<Person>() {
 			public Object execute(Person p) { return p.male ? "M" : "F"; }
+		}));
+		
+		// cs people query
+		printList(cs_people.Where(new IPredicate<Person>(){
+			public Boolean execute(Person p) { return p.phone % 2 == 0; }
+		}).Sort( new ISortDelegate<Person>() {
+			public Object execute(Person p) { return p.male ? "M" : "F"; }
+		}).Sort( new ISortDelegate<Person>() {
+			public Object execute(Person p) { return p.last; }
+		}).Select(new IDelegate<String, Person>() {
+			public String execute(Person p) { return p.first; }
 		}));
 	}
 	
